5628cc624d06c44762a25761bc465ceb2998f9fe
tim <jan-brenner@gmx.de>
2017-08-02 18:18:43 +0200

    Updated ILanguage interface.

--- Language/Language/Interfaces/ILanguage.cs
+++ Language/Language/Interfaces/ILanguage.cs
@@ -13,24 +13,42 @@ namespace Languages.Interfaces
         ///     The identifier of the <see cref="Language" /> according to
         ///     https://msdn.microsoft.com/de-de/library/ee825488(v=cs.20).aspx
         /// </summary>
+        // ReSharper disable once UnusedMemberInSuper.Global
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once UnusedMemberInSuper.Global
         string Identifier { get; set; }
+
+        /// <summary>
+        /// The name of the <see cref="Language" />
+        /// </summary>
+        // ReSharper disable once UnusedMemberInSuper.Global
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once UnusedMemberInSuper.Global
+        string Name { get; set; }
 
         /// <summary>
         ///     A <see cref="List{T}" /> of <see cref="Word" />s in the loaded <see cref="Language" />
         /// </summary>
+        // ReSharper disable once UnusedMemberInSuper.Global
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once UnusedMemberInSuper.Global
         List<Word> Words { get; set; }
 
         /// <summary>
         ///     Gets a word by a key
         /// </summary>
         /// <param name="key">The key that is defined for the <see cref="Word" /></param>
-        /// <returns></returns>
+        /// <returns>The <see cref="Word" /> defined by the key.</returns>
+        /// // ReSharper disable once UnusedMemberInSuper.Global
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once UnusedMemberInSuper.Global
         string GetWord(string key);
 
         /// <summary>
         ///     Gets the current <see cref="CultureInfo" />
         /// </summary>
         /// <returns>The currently loaded <see cref="CultureInfo" /></returns>
+        // ReSharper disable once UnusedMember.Global
         CultureInfo GetCulture();
     }
 }
