7d4aae38b94ed55a17f766219eb82c8cbae5488b
SeppPenner <HansM2013@gmx.de>
2019-09-29 19:07:20 +0200

    Updated tag information.

--- Language/build.bat
+++ Language/build.bat
@@ -1,4 +1,4 @@
 dotnet build Language.sln -c Release
 xcopy /s .\Language\bin\Release ..\Nuget\Source\
-xcopy /s .\Documentation\ ..\Nuget\Documentation\
+xcopy /s .\Documentation ..\Nuget\Documentation\
 pause
