3ca7a876e558a52ad5c3ee3d402b0bc244092515
SeppPenner <HansM2013@gmx.de>
2022-02-21 16:33:29 +0100

    Updated build script.

--- PushPackage.bat
+++
@@ -1,3 +0,0 @@
-dotnet nuget push "src\Language\bin\Release\HaemmerElectronics.SeppPenner.Language.*.nupkg" -s "github" --skip-duplicate
-dotnet nuget push "src\Language\bin\Release\HaemmerElectronics.SeppPenner.Language.*.nupkg" -s "nuget.org" --skip-duplicate -k "%NUGET_API_KEY%"
-PAUSE
